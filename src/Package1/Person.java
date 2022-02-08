bbad383c9c6bcc595b657fc4c33fc1c168fa2a9c
Yeiner Hidalgo Molina <yeinerhidalgomolina@gmail.com>
2022-02-07 21:51:44 -0500

    La clase person esta documentada

--- src/Package1/Person.java
+++ src/Package1/Person.java
@@ -33,43 +33,73 @@ public class Person {
         this.dateBirth = dateBirth;
         this.height = height;
     }
-
+    /**
+     * Modifica el parametro name
+     * @return retorna un String de name
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * Muestra el parametro name
+     * @param name 
+     */
     public void setName(String name) {
         this.name = name;
     }
-
+    /**
+     * Modifica el parametro lastName1
+     * @return retorna un String de lastName1
+     */
     public String getLastName1() {
         return lastName1;
     }
-
+    /**
+     * Muestra el parametro lastName
+     * @param lastName1 
+     */
     public void setLastName1(String lastName1) {
         this.lastName1 = lastName1;
     }
-
+    /**
+     * Modifica el parametro lastName2
+     * @return retorna un String de lastName2
+     */
     public String getLastName2() {
         return lastName2;
     }
-
+    /**
+     * Muestra el parametrolstName2
+     * @param lastName2 
+     */
     public void setLastName2(String lastName2) {
         this.lastName2 = lastName2;
     }
-
+    /**
+     * Modifica el parametro dateBirth
+     * @return retorna Date de dateBirth
+     */
     public Date getDateBirth() {
         return dateBirth;
     }
-
+    /**
+     * Muestra el parametro dateBirth
+     * @param dateBirth 
+     */
     public void setDateBirth(Date dateBirth) {
         this.dateBirth = dateBirth;
     }
-
+    /**
+     * Modifica el parametro height
+     * @return retorna un float de height
+     */
     public float getHeight() {
         return height;
     }
-
+    /**
+     * Muestra el parametro height
+     * @param height 
+     */
     public void setHeight(float height) {
         this.height = height;
     }
